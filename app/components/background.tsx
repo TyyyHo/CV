229b63db2d19031c736d417de745b474d9fb71df
Tyler <ty.ho1126@gmail.com>
2025-03-01 16:39:50 +0800

    background

---
+++ app/components/background.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Aurora from "@/reactbits/Backgrounds/Aurora/Aurora";
+
+export default function Background() {
+  return (
+    <div className="fixed inset-0 -z-10">
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={1}
+        amplitude={0.3}
+        speed={1}
+      />
+    </div>
+  );
+}
